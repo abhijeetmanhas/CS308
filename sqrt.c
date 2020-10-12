dc6b05d6731904e3d19270d9ea6c02efe3cf7c94
abhijeetmanhas <abhijeetmanhas720@gmail.com>
2020-10-13 01:40:01 +0530

    added support for negative ints

--- sqrt.c
+++ sqrt.c
@@ -43,7 +43,13 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	if(argv[1][0]=='-')
+	{
+		input=-input;
+		printf("Sqrt of %d is %fi\n",input,sqrt(input));
+	}
+	else
+		printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting.");
 	return(0);
 
