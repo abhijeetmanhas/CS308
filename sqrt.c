94cf354a845d2b9112909d8737cfda83eef013b9
abhijeetmanhas <abhijeetmanhas720@gmail.com>
2020-10-13 01:32:51 +0530

    check input is valid int

--- sqrt.c
+++ sqrt.c
@@ -4,11 +4,41 @@ positive number. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
+int is_valid_int(const char *str)
+{
+   // Handle negative numbers.
+   //
+   if (*str == '-')
+      ++str;
+
+   // Handle empty string or just "-".
+   //
+   if (!*str)
+      return 0;
+
+   // Check for non-digit chars in the rest of the stirng.
+   //
+   while (*str)
+   {
+      if (!isdigit(*str))
+         return 0;
+      else
+         ++str;
+   }
+
+   return 1;
+}
 // this is a comment
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
 		printf("Usage: sqrt input\n\n");
+		exit(-1);
+	}
+	if(!is_valid_int(argv[1]))
+	{
+		printf("Usage: Input should be a valid number\n\n");
 		exit(-1);
 	}
 
